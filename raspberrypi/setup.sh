5557acaa9bd19671e168c231dd3997482bdd5ec0
Jeff Rade <jeffrade@gmail.com>
2022-12-14 16:57:46 -0600

    Adds gnome-disks for formatting and checks for existence before install

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -2,7 +2,17 @@
 
 set -e
 
-sudo apt install rpi-imager
+sudo apt update
+
+if [ ! "$(command -v gnome-disks)" ]; then
+ sudo apt install gnome-disk-utility
+fi
+
+echo "Run gnome-disks if you need to format your sd card"
+
+if [ ! "$(command -v rpi-imager)" ]; then
+ sudo apt install rpi-imager
+fi
 
 echo "Make sure to enable SSH"
 
@@ -12,3 +22,4 @@ echo "Now ssh into your raspberry pi:"
 echo "'ssh pi@raspberrypi.local'"
 echo "Then run the following command to install pi-hole:"
 echo "'sudo curl -sSL https://install.pi-hole.net | bash'"
+echo "See more at https://github.com/pi-hole/pi-hole/#one-step-automated-install"
